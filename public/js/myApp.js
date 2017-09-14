37c3dfe0393276bdc0381449282a66d1f18168e8
BK <bkam03@yahoo.com>
2017-09-13 20:22:46 -1000

    set scope for myModel

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -9,8 +9,11 @@ var myApp = angular.module('myApp');
 
 
 //name controller, define dependencies in array.
+//can only scope from controller in this scope.
+//put dependencies before array.  if scope is first in array, scope must be first in function args.
 myApp.controller( 'myController', [ '$scope', function( $scope ) {
   $scope.myFirstName = 'Brandon';
+  $scope.myModel= 'Ready Player One';
 } ] );
 
 myApp
