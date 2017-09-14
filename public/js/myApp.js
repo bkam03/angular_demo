141d125fc382dda2e34afb70ad58de9acf036a94
BK <bkam03@yahoo.com>
2017-09-13 19:47:44 -1000

    angular js file created

---
+++ public/js/myApp.js
@@ -0,0 +1,15 @@
+// creation uses a 2nd array argument to import dependencies
+//pass in 2 args, create new module.  module called myApp.  array is list of dependencies to pass into module. must pass in second parameter or everything breaks.
+angular.module('myApp', []);
+
+// retrieval has only one argument
+//retrieve created module from angular.
+var myApp = angular.module('myApp');
+//bootstrapping process.
+myApp
+    .config(function(){
+        // config
+    })
+    .run(function(){
+        // initialize
+    });
