12b02e714c4c697ef517e43ab992131494a9a133
BK <bkam03@yahoo.com>
2017-09-13 20:32:36 -1000

    removed from global scope

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,22 +1,13 @@
+
+
 // creation uses a 2nd array argument to import dependencies
 //pass in 2 args, create new module.  module called myApp.  array is list of dependencies to pass into module. must pass in second parameter or everything breaks.
 angular.module('myApp', []);
 
 // retrieval has only one argument
 //retrieve created module from angular.
-var myApp = angular.module('myApp');
+angular.module('myApp')
 //bootstrapping process.
-
-
-//name controller, define dependencies in array.
-//can only scope from controller in this scope.
-//put dependencies before array.  if scope is first in array, scope must be first in function args.
-myApp.controller( 'myController', [ '$scope', function( $scope ) {
-  $scope.myFirstName = 'Brandon';
-  $scope.myModel= 'Ready Player One';
-} ] );
-
-myApp
     .config(function(){
         // config
     })
