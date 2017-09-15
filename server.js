eaf48ebedc3b0c601e10fb968409349f78096d74
BK <bkam03@yahoo.com>
2017-09-14 18:30:59 -1000

    added api endpoint

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const app = express();
 
 app.use( express.static( 'public' ) );
 
+app.get( '/api', ( req, res ) => {
+  return res.json( [
+  {
+    title: 'The Other Guys',
+    year: 2008
+  },
+  {
+    title: 'It',
+    year: 1990
+  },
+  {
+    title: 'Wolf of Wall Street',
+    year: 2015
+  }
+  ] );
+} );
+
 
 const server = app.listen( PORT, () => {
   console.log( `server running on ${ PORT }` );
